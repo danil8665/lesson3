4fb17da6ca3648a761f77e11d35691b3e5c54a96
danil8665 <danilfilatov319@gmail.com>
2022-01-21 19:27:51 +0200

    stashedit

--- ConsoleApplicationOOP/WorkClass.cpp
+++ ConsoleApplicationOOP/WorkClass.cpp
@@ -1,18 +1,15 @@
 #include "WorkClass.h"
 void WorkClass::startWork() {
     std::cout << "Hello Привет World!\n";
-	
+
 
     GoodBasic* universalPointer01 = new Chipsi;
-    
+
 
     static_cast<GoodBasic*>(universalPointer01)->toString();
-<<<<<<< Updated upstream
-    
-=======
+
     Freezer freezer;
     freezer.toString();
     Software software;
     software.toString();
->>>>>>> Stashed changes
 }
